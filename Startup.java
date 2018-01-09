23237c78e9bdd719f07a12ad49690a41301fd086
LC15 <clabiba97@gmail.com>
2018-01-09 14:39:02 -0500

    -a

---
+++ Startup.java
@@ -0,0 +1,18 @@
+public class Startup{
+	public static void main(String[] args) {
+       
+    }
+    public String email= "no email address";
+    public complete= false;
+   
+    public setEmailAddress(String CompanyEmail){
+    	email=CompanyEmail;
+    }
+    public String showEmail(){
+    	return email;
+    }
+    public CompletedSurvey(){
+
+    }
+
+}

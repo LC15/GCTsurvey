173aa9ee4bad3b941ce496f57fa65c2680cea871
LC15 <clabiba97@gmail.com>
2018-01-10 14:06:36 -0500

    completed program

--- createEmailList.java
+++ createEmailList.java
@@ -1,6 +1,123 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.util.Scanner;
+import java.io.*; 
+
+import java.nio.charset.StandardCharsets;
+
 public class createEmailList{
+	//initialize class variables
+	static ArrayList <String> accelerators = new ArrayList <String> ();
+	static ArrayList <String> responded = new ArrayList <String> ();
+	static ArrayList <String> emailsList= new ArrayList <String> ();
+	static String emails="";
+
 	public static void main(String[] args) {
        System.out.println("Create Email is compiling");
+
+       createCompanyList();
+       createRespondedList();
+       creatAcceleratorEmails();
+       createEmailList();
+     
     }
-	public string EmailingList(){}    
+	public static void createCompanyList(){
+		
+		File acceleratorsFile= new File("AcceleratorStartups.txt");
+		
+		Scanner scanner = null;
+		
+		try {
+			scanner = new Scanner(acceleratorsFile);	
+			scanner.useDelimiter("\n *");
+			
+			while(scanner.hasNext()){
+				accelerators.add(scanner.next());
+		}
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}    
+
+	public static void createRespondedList(){
+		
+		File respondedFile= new File("CompaniesWhoResponded.txt");
+		
+		Scanner scannerTwo = null;
+		boolean checking;
+		try {
+			scannerTwo = new Scanner(respondedFile);	
+			scannerTwo.useDelimiter("\n *");
+			
+			while(scannerTwo.hasNext()){
+				responded.add(scannerTwo.next());
+		}
+				
+			/*	checking=false;
+				for(String accelerator:accelerators){*/
+
+			/*		if(accelerator.equals(scannerTwo.next())){
+						System.out.println( "completed survey");
+						checking=true;
+					}
+				}
+				if(!checking){
+					System.out.println( " has NOT completed survey");
+				}
+			}*/
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void creatAcceleratorEmails(){
+		File emailsFile= new File("emails.txt");
+		
+		Scanner scannerThree = null;
+		
+		try {
+			scannerThree = new Scanner(emailsFile);	
+			scannerThree.useDelimiter("\n *");
+			
+			while(scannerThree.hasNext()){
+				emailsList.add(scannerThree.next());
+		}
+		}catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void createEmailList(){
+		boolean check;
+		int pos=0;
+		int didntDoIt=0;
+		for(String accelerator: accelerators){
+			
+			check=false;
+			for(String company: responded){
+				if(accelerator.equals(company)){
+					check=true;
+					
+					//System.out.println(accelerator);
+					break;
+				}
+				
+			}
+			if(!check){
+				System.out.println(emailsList.get(pos));
+			}
+			pos++;
+		}
+	
+	}
+
+
+
+
+
 }
